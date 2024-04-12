92eac56777cc2adb0f96ca0462c328a3cfe20702
Marc Sportiello <mesportiello@gmail.com>
2024-04-12 17:08:05 +0200

    Initialize development environment

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const { exec } = require('child_process');
+
+module.exports = (env, argv) => {
+    const isDevelopment = argv.mode === 'development';
+
+    return {
+        mode: isDevelopment ? 'development' : 'production',
+        devtool: isDevelopment ? 'inline-source-map' : undefined,
+        entry: {
+            main: './src/index.ts'
+        },
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: 'nitro.js'
+        },
+        resolve: {
+            extensions: ['.ts', '.js'],
+            alias: {
+                '@': path.resolve(__dirname, 'src')
+            }
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        plugins: [{
+            apply: (compiler) => {
+                if (!isDevelopment) return;
+                compiler.hooks.afterEmit.tap('AfterEmitPlugin', (compilation) => {
+                    exec('npm run start', (err, stdout, stderr) => {
+                        if (stdout) process.stdout.write(stdout);
+                        if (stderr) process.stderr.write(stderr);
+                    });
+                });
+            }
+        }]
+    }
+}
